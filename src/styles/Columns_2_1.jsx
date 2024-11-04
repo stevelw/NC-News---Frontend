9fbdd69d2e91e07665404b16d0c0700960065bf5
stevelw <steve@stevelw.co.uk>
2024-11-04 16:50:12 +0000

    rf: extract grid to css file

--- src/styles/Columns_2_1.jsx
+++ src/styles/Columns_2_1.jsx
@@ -1,13 +1,3 @@
 export default function Columns_2_1({ children }) {
-  const style = {
-    display: "grid",
-    gridTemplateColumns: "1fr",
-    gridTemplateRows: 'auto',
-    gridTemplateAreas: `
-        'grid1'
-        'grid2'
-        'grid3'
-        `,
-  };
-  return <div style={style}>{children}</div>;
+  return <div className="columns-2-1">{children}</div>;
 }
