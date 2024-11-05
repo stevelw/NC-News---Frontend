2f28bbbd81e77cdd2cc777c904ae26e7fff3220c
stevelw <steve@stevelw.co.uk>
2024-11-05 12:51:33 +0000

    hf: image source embed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,5 @@
+import logo from "../assets/logo.png";
+
 export default function Header() {
   const style = {
     display: "flex",
@@ -6,11 +8,7 @@ export default function Header() {
   };
   return (
     <div style={style}>
-      <img
-        src="src/assets/logo.png"
-        alt="The logo for NC News"
-        width={"100px"}
-      />
+      <img src={logo} alt="The logo for NC News" width={"100px"} />
       <h1>NC News - When it happens</h1>
     </div>
   );
