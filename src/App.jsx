28752dde60b8a441fe2c1bef2c18e89f85a79600
stevelw <steve@stevelw.co.uk>
2024-11-05 12:15:05 +0000

    enable links to articals

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import LatestNews from "./components/LatestNews";
+import Artical from "./components/Article";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LatestNews />} />
+      <Route path="/articles/:articleUrl" element={<Artical />} />
     </Routes>
   );
 }
---
+++ src/components/Article.jsx
@@ -0,0 +1,6 @@
+export default function Artical() {
+    const style = {
+        border: 'solid red 1px'
+    }
+    return <p style={style}>Artical</p>
+}
