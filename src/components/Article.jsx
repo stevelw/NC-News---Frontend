f3565f6a16b55186bc8c448712f045043568a3cf
stevelw <steve@stevelw.co.uk>
2024-11-05 13:26:45 +0000

    Populate article page from mock network call

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,6 +1,11 @@
+import { useParams } from "react-router-dom";
 import CommentList from "./CommentList";
 import Header from "./Header";
 import Vote from "./Vote";
+import { getArtical } from "../utils/api";
+import { useContext } from "react";
+import { TopicsContext } from "../contexts/Topics";
+
 export default function Article() {
   const backButtonStyle = {
     gridArea: "top-left-button",
@@ -12,26 +17,40 @@ export default function Article() {
     border: "red solid 1px",
   };
   const headlineStyle = { gridArea: "headline" };
-  const topicStyle = { gridArea: "topic", textAlign: "right" };
-  const timestampStyle = { gridArea: "timestamp", textAlign: "right" };
+  const topicStyle = {
+    gridArea: "topic",
+    textAlign: "right",
+  };
+  const timestampStyle = {
+    gridArea: "timestamp",
+    textAlign: "right",
+  };
   const imageStyle = { gridArea: "image" };
   const bodyStyle = { gridArea: "body" };
-  const authorStyle = { gridArea: "author", textAlign: "right" };
+  const authorStyle = {
+    gridArea: "author",
+    textAlign: "right",
+  };
+
+  const articleId = useParams().articleUrl.match(/(?<=-)[^-]+$/);
+  const { topics, setTopics } = useContext(TopicsContext);
+
+  const { author, title, body, topic, created_at, article_img_url } =
+    getArtical();
+  const timestamp = new Date(created_at).toDateString();
 
   return (
     <div className="article-grid">
       <p style={backButtonStyle}>Back button</p>
       <p style={homeButtonStyle}>Home button</p>
-      <h2 style={headlineStyle}>Headline</h2>
-      <p style={topicStyle}>Topic</p>
-      <p style={timestampStyle}>date/time</p>
-      <img
-        style={imageStyle}
-        src="https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg"
-        alt=""
-      />
-      <p style={bodyStyle}>body</p>
-      <p style={authorStyle}>author</p>
+      <h2 style={headlineStyle}>{title}</h2>
+      <p style={topicStyle}>{topics[topic] ?? topic}</p>
+      <p style={timestampStyle}>{timestamp}</p>
+      <img style={imageStyle} src={article_img_url} alt="" />
+      <div style={bodyStyle}>
+        <p>{body}</p>
+      </div>
+      <p style={authorStyle}>{author}</p>
       <Vote></Vote>
       <CommentList />
     </div>
