be949882758d2ead97f3afe4b1d41a54a267df70
stevelw <steve@stevelw.co.uk>
2024-11-07 09:17:33 +0000

    post comment to api

--- src/components/CommentComposer.jsx
+++ src/components/CommentComposer.jsx
@@ -1,14 +1,40 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import { postComment } from "../utils/api";
+import { useParams } from "react-router-dom";
 import { UserContext } from "../contexts/UserContext";
 
 export default function CommentComposer() {
   const {user} = useContext(UserContext)
+  const articleId = useParams().articleUrl.match(/(?<=-)[^-]+$/);
+  const [commentInput, setCommentInput] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    postComment(articleId, commentInput, user.username)
+      .then(() => {
+        setCommentInput("");
+      })
+      .catch((err) => {});
+  }
 
   return (
-    <form style={{gridArea: 'comment'}}>
-      <button className="leave-a-comment-button">Leave a comment</button>
-      <label hidden htmlFor="comment-input">Comment</label>
-      <textarea id="comment-input" className="comment-input"></textarea>
+    <form onSubmit={handleSubmit} style={{ gridArea: "comment" }}>
+      <button
+        type="submit"
+        disabled={!commentInput}
+        className="leave-a-comment-button"
+      >
+        Leave a comment
+      </button>
+      <label hidden htmlFor="comment-input">
+        Comment
+      </label>
+      <textarea
+        id="comment-input"
+        value={commentInput}
+        onChange={({ target: { value } }) => setCommentInput(value)}
+        className="comment-input"
+      ></textarea>
     </form>
   );
 }
