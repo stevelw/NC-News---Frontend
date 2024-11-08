6a21b050b301137a41b06cc2899f2ebfcd9ef64b
stevelw <steve@stevelw.co.uk>
2024-11-08 04:30:30 +0000

    rf: extract article card children to component

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,4 +1,25 @@
-export default function ArticleCard({ children }) {
+import { Link } from "react-router-dom";
+
+export default function ArticleCard({
+  article: {
+    article_id,
+    title,
+    topic,
+    article_img_url,
+    created_at,
+    comment_count,
+    votes,
+  },
+  topics,
+  isTopicsLoading,
+  isTopicsError,
+}) {
+  const urlFriendlyTitle = title.replaceAll(/[^a-z]/gi, "-");
+  const articleUrl = "/articles/" + urlFriendlyTitle + "-" + article_id;
+  const style = {
+    maxWidth: "45%",
+  };
+
   return (
     <div
       style={{
@@ -7,7 +28,24 @@ export default function ArticleCard({ children }) {
         margin: "10px",
       }}
     >
-      {children}
+      <div style={style}>
+        <Link to={articleUrl}>
+          <h3>{title}</h3>
+        </Link>
+        {isTopicsError || isTopicsLoading ? (
+          <p>{topic}</p>
+        ) : (
+          <Link to={"/topics/" + topic}>
+            <p>{topics[topic]}</p>
+          </Link>
+        )}
+        <p>{new Date(created_at).toDateString()}</p>
+        <p>
+          {comment_count} comment{comment_count > 1 && "s"}
+        </p>
+        <p>&#128077; {votes}</p>
+      </div>
+      <img style={style} src={article_img_url} alt="" />
     </div>
   );
 }
--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -2,7 +2,7 @@ import ArticleCard from "./ArticleCard";
 import { getArticles } from "../utils/api";
 import { useEffect, useState } from "react";
 import ErrorComponent from "./ErrorComponent";
-import { Link, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import Sorting from "./Sorting";
 
 export default function ArticleList({
@@ -11,10 +11,6 @@ export default function ArticleList({
   isTopicsError,
   filterTopic,
 }) {
-  const childStyle = {
-    maxWidth: "45%",
-  };
-
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
@@ -68,43 +64,17 @@ export default function ArticleList({
             .filter(({ topic }) => {
               return !filterTopic || topic === filterTopic;
             })
-            .map(
-              ({
-                article_id,
-                title,
-                topic,
-                article_img_url,
-                created_at,
-                comment_count,
-                votes,
-              }) => {
-                const urlFriendlyTitle = title.replaceAll(/[^a-z]/gi, "-");
-                const articleUrl =
-                  "/articles/" + urlFriendlyTitle + "-" + article_id;
-                return (
-                  <ArticleCard key={article_id}>
-                    <div style={childStyle}>
-                      <Link to={articleUrl}>
-                        <h3>{title}</h3>
-                      </Link>
-                      {isTopicsError || isTopicsLoading ? (
-                        <p>{topic}</p>
-                      ) : (
-                        <Link to={"/topics/" + topic}>
-                          <p>{topics[topic]}</p>
-                        </Link>
-                      )}
-                      <p>{new Date(created_at).toDateString()}</p>
-                      <p>
-                        {comment_count} comment{comment_count > 1 && "s"}
-                      </p>
-                      <p>&#128077; {votes}</p>
-                    </div>
-                    <img style={childStyle} src={article_img_url} alt="" />
-                  </ArticleCard>
-                );
-              }
-            )}
+            .map((article) => {
+              return (
+                <ArticleCard
+                  key={article.article_id}
+                  article={article}
+                  topics={topics}
+                  isTopicsLoading={isTopicsLoading}
+                  isTopicsError={isTopicsError}
+                />
+              );
+            })}
         </div>
       )}
     </>
