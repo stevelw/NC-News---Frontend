53b4a0a29317e3151dbb98a93f33d2c655235bc0
stevelw <steve@stevelw.co.uk>
2024-11-09 22:22:56 +0000

    migrate LoadButton css to BEM

--- src/components/LoadButton.jsx
+++ src/components/LoadButton.jsx
@@ -1,11 +1,6 @@
 export default function LoadButton() {
   return (
-    <div
-      style={{
-        gridArea: "grid2",
-        border: "red solid 1px",
-      }}
-    >
+    <div className="load-button">
       <p>LoadButton</p>
     </div>
   );
