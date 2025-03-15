98ebc8470cd42c7a567c7fb42c6b575cc64cc436
stevelw <steve@stevelw.co.uk>
2025-03-15 11:19:12 +0000

    add report new news button to header

--- src/components/HeaderElement.jsx
+++ src/components/HeaderElement.jsx
@@ -1,10 +1,18 @@
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
 
 export default function HeaderElement() {
   return (
     <div className="header">
       <img src={logo} alt="The logo for NC News" width={"100px"} />
-      <h1>NC News - When it happens</h1>
+      <div>
+        <h1>NC News - When it happens</h1>
+        <Link to={"/report"}>
+          <button type="button" className="header__report-button">
+            Report your news
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
